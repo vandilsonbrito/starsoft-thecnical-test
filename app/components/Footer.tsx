4da6c345298e8397419a32921c8cdaf1e6e575bd
vandilsonbrito <vandilson25@outlook.com>
2024-10-25 21:57:51 -0300

    Feat: create 'Footer' component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/footer.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+        <p>STARSOFT &copy; TODOS OS DIREITOS RESERVADOS</p>
+    </footer>
+  )
+}
